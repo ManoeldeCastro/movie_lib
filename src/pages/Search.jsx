be36e52e7403d57a079d2f4cc9bc961430b6b77b
manoel <manoeldiasmf@outlook.com>
2023-01-15 19:01:12 -0300

    Update Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 
-const searchURL = import.meta.env.VITE_SEARCH;
+const searchURL = 'https://api.themoviedb.org/3/search/movie/';
 const apiKey = import.meta.env.VITE_API_KEY;
 
 import './MoviesGrid.css';
